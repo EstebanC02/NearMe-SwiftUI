73dc8543ae22159a84f0227b082540d28c063068
Esteban <esteban.castiblanco@pm.me>
2021-07-29 19:09:18 -0500

    Initial commit

---
+++ NearMeSwiftUI/NearMeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NearMeSwiftUI
+//
+//  Created by Esteban Moncaleano on 29/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
